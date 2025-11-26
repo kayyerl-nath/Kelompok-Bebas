1fe792f3a705c31c67ed370eba3f3ff88576a0d9
joveeranuntu <joveeranuntu026@student.unsrat.ac.id>
2025-11-26 14:14:44 +0800

    Update Kelompok-Bebas.py

--- Kelompok-Bebas.py
+++ Kelompok-Bebas.py
@@ -19,8 +19,22 @@ class Food:
 def next_turn():
   pass
 
-def change_direction():
-  pass
+def change_direction(new_direction):
+
+    global direction
+
+    if new_direction == 'left':
+        if direction != 'right':
+            direction = new_direction
+    elif new_direction == 'right':
+        if direction != 'left':
+            direction = new_direction
+    elif new_direction == 'up':
+        if direction != 'down':
+            direction = new_direction
+    elif new_direction == 'down':
+        if direction != 'up':
+            direction = new_direction
 
 def check_collisiions(snake):
 
