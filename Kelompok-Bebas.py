4d92f37808d3edb45282555669b22cfa3bee4a57
kayyerl-nath <nathanfrans28@gmail.com>
2025-11-26 13:04:37 +0800

    Set up Snake game framework with Tkinter
    Initialize a basic structure for a Snake game using Tkinter.

--- Kelompok-Bebas.py
+++ Kelompok-Bebas.py
@@ -1,1 +1,46 @@
-print("hello world")
+from tkinter import *
+import random
+
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#e5de00"
+FOOD_COLOR = "#de0a26"
+BACKGROUND_COLOR = "#000000"
+
+class Snake:
+  pass 
+
+class Food:
+  pass
+
+def next_turn():
+  pass
+
+def change_direction():
+  pass
+
+def check_collisions():
+  pass
+
+def game_over():
+  pass
+
+
+window = Tk()
+window.title("Snake game")
+window.resizable(False, False)
+
+score = 0
+direction = 'down'
+
+
+label = Label(window, text="Score.{}".format(score), font = ('Consolas'))
+label.pack()
+
+canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
+canvas.pack()
+
+window.mainloop()
